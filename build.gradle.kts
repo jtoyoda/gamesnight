bf358525ae1a8b7faf0dca48ec773b159fb3590b
Jamie Toyoda <jtoyoda@openhouse.ai>
2021-06-19 18:15:28 -0600

    Using java 11 standard lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	implementation("org.flywaydb:flyway-core")
     implementation("com.sendgrid:sendgrid-java:4.0.1")
 
